c0f9e2d8b83973ae22efb19c49bf602a77306bb1
Cargill Seiveright <microcargill@hotmail.com>
2020-04-24 21:03:02 -0500

    create first Vue component

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -41,6 +41,22 @@ Vue.component('app-footer', {
     }
 })
 
+Vue.component('news-list', {
+  template: `
+  <div class="news">
+    <h2>News</h2>
+      <ul class="news__list">
+        <li class="news__item">News item 1</li>
+        <li class="news__item">News item 2</li>
+        <li class="news__item">News item 3</li>
+      </ul>
+  </div>
+  `,
+  data: function() {
+      return {}
+  }
+})
+
 
 let app = new Vue({
     el: '#app',
